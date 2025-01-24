74a8cbaef878a2286873149a7bc329890fc578b4
Zhaoyu <Airtual31959@gmail.com>
2025-01-24 20:28:38 +0800

    tables

---
+++ sql/freshy.sql
@@ -0,0 +1,219 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80039
+ Source Host           : localhost:3306
+ Source Schema         : freshy
+
+ Target Server Type    : MySQL
+ Target Server Version : 80039
+ File Encoding         : 65001
+
+ Date: 24/01/2025 20:16:55
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
+  `admin_id` int UNSIGNED NOT NULL COMMENT '管理员ID',
+  `admin_password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '管理员密码',
+  INDEX `admin_id`(`admin_id` ASC) USING BTREE,
+  CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for campus
+-- ----------------------------
+DROP TABLE IF EXISTS `campus`;
+CREATE TABLE `campus`  (
+  `campus_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '校区ID',
+  `campus_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '校区名称',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '校区创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`campus_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for classes
+-- ----------------------------
+DROP TABLE IF EXISTS `classes`;
+CREATE TABLE `classes`  (
+  `class_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '班级ID',
+  `class_number` smallint NOT NULL COMMENT '班级号',
+  `major_id` int UNSIGNED NOT NULL COMMENT '专业ID',
+  `college_id` int UNSIGNED NOT NULL COMMENT '学院ID',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '班级创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`class_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for classrooms
+-- ----------------------------
+DROP TABLE IF EXISTS `classrooms`;
+CREATE TABLE `classrooms`  (
+  `classroom_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '教室ID',
+  `classroom_number` int UNSIGNED NOT NULL COMMENT '教室号',
+  `classroom_type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '教室类型（办公室/教室）',
+  `classroom_seats` int NOT NULL COMMENT '教室座位数',
+  `teaching_building` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '教学楼名称',
+  `campus_id` int UNSIGNED NOT NULL COMMENT '校区ID',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '教室创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`classroom_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for colleges
+-- ----------------------------
+DROP TABLE IF EXISTS `colleges`;
+CREATE TABLE `colleges`  (
+  `college_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '学院ID',
+  `college_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '学院名称',
+  `college_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '学院信息',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '学院创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`college_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for course_user
+-- ----------------------------
+DROP TABLE IF EXISTS `course_user`;
+CREATE TABLE `course_user`  (
+  `course_id` int UNSIGNED NOT NULL COMMENT '课程ID',
+  `user_id` int UNSIGNED NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`course_id`, `user_id`) USING BTREE,
+  INDEX `user_id`(`user_id` ASC) USING BTREE,
+  CONSTRAINT `course_user_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `course_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for courses
+-- ----------------------------
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE `courses`  (
+  `course_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '课程ID',
+  `course_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '课程名称',
+  `course_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '课程信息',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '课程创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`course_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for dormitories
+-- ----------------------------
+DROP TABLE IF EXISTS `dormitories`;
+CREATE TABLE `dormitories`  (
+  `dormitory_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '宿舍ID',
+  `dormitory_number` int UNSIGNED NOT NULL COMMENT '宿舍号',
+  `dormitory_building` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '宿舍楼名称',
+  `campus_id` int UNSIGNED NOT NULL COMMENT '校区ID',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '宿舍创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`dormitory_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for freshmen
+-- ----------------------------
+DROP TABLE IF EXISTS `freshmen`;
+CREATE TABLE `freshmen`  (
+  `freshman_id` int UNSIGNED NOT NULL COMMENT '新生ID',
+  `freshman_number` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '新生学号',
+  `freshman_password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '新生密码',
+  `enrollment_date` date NOT NULL COMMENT '入学时间',
+  UNIQUE INDEX `freshman_number`(`freshman_number` ASC) USING BTREE,
+  INDEX `freshman_id`(`freshman_id` ASC) USING BTREE,
+  CONSTRAINT `freshmen_ibfk_1` FOREIGN KEY (`freshman_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for major_user
+-- ----------------------------
+DROP TABLE IF EXISTS `major_user`;
+CREATE TABLE `major_user`  (
+  `major_id` int UNSIGNED NOT NULL COMMENT '专业ID',
+  `user_id` int UNSIGNED NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`major_id`, `user_id`) USING BTREE,
+  INDEX `user_id`(`user_id` ASC) USING BTREE,
+  CONSTRAINT `major_user_ibfk_1` FOREIGN KEY (`major_id`) REFERENCES `majors` (`major_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `major_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for majors
+-- ----------------------------
+DROP TABLE IF EXISTS `majors`;
+CREATE TABLE `majors`  (
+  `major_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '专业ID',
+  `major_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '专业名称',
+  `major_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '专业信息',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '专业创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`major_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for roles
+-- ----------------------------
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles`  (
+  `role_id` tinyint NOT NULL COMMENT '角色ID',
+  `role_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT 'freshman' COMMENT '角色名称',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '角色创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for teachers
+-- ----------------------------
+DROP TABLE IF EXISTS `teachers`;
+CREATE TABLE `teachers`  (
+  `teacher_id` int UNSIGNED NOT NULL COMMENT '教师ID',
+  `teacher_password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '教师密码',
+  `email` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '教师邮箱',
+  `hire_date` date NOT NULL COMMENT '雇用日期',
+  UNIQUE INDEX `email`(`email` ASC) USING BTREE,
+  INDEX `teacher_id`(`teacher_id` ASC) USING BTREE,
+  CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `user_id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `user_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户姓名',
+  `gender` enum('M','F','NA') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户性别',
+  `birth_date` date NOT NULL COMMENT '用户出生日期',
+  `role_id` tinyint NOT NULL COMMENT '用户角色',
+  `id_number` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户身份证号',
+  `phone_number` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户手机号',
+  `address` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户住址',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '用户创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '删除标识',
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
